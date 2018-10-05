2595905e6a27772f4311231dde70460651706cc7
MikeyB Tablet <michaelbrewster11@yahoo.co.uk>
2018-10-05 08:51:08 +0100

    start of section 5

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,8 +1,3 @@
-// "PROD" creds
-// clientID -- 501288438778-tfrfacpd42ban4iit4c3f606cfdc3c6e.apps.googleusercontent.com
-// clientSecret -- OllJ4pSpLql8bRZZ_AG6VaZc
-// mongoURL -- need to create
-
 // pulling env vars from HEROKU
 // these will need to be set up on the hosting server
 /*
