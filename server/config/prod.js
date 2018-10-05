430085c1ca1236335fd45241995ff1f22de8976f
MikeyB Tablet <michaelbrewster11@yahoo.co.uk>
2018-10-05 08:40:22 +0100

    start of section 5

---
+++ server/config/prod.js
@@ -0,0 +1,15 @@
+// "PROD" creds
+// clientID -- 501288438778-tfrfacpd42ban4iit4c3f606cfdc3c6e.apps.googleusercontent.com
+// clientSecret -- OllJ4pSpLql8bRZZ_AG6VaZc
+// mongoURL -- need to create
+
+// pulling env vars from HEROKU
+// these will need to be set up on the hosting server
+/*
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLINET_SECRET,
+  mongoURL: process.env.MONGO_URL,
+  cookieKey: process.ev.COOKIE_KEY
+};
+*/
