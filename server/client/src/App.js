9f0d5a236e33d66f967d66392105103fc6a7551a
mikeyb_tablet <mikeyb_tablet>
2018-10-08 19:33:29 +0100

    settingup tablet again

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -14,8 +14,6 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <a href="/auth/google">Sign In With Google</a>
-        </p>
-        <a href="/auth/google">Sign In With Google</a>
       </div>
     );
   }
