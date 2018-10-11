38e2593549089db2874110096ece2dd20d0fcb78
mikeyb_tablet <mikeyb_tablet>
2018-10-11 07:57:10 +0100

    Section7 save work

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
+// action creators
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
 
