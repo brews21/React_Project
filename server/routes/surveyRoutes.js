1abc50054a9cbcc2a7ec53ce9a93e47acc5cfc62
mikeyb_tablet <mikeyb_tablet>
2018-10-17 19:47:37 +0100

    Section10 save

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,11 +1,23 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
 const requireLogin = require("../middleware/requireCredits");
 
+const Survey = mongoose.model("survey");
+
 module.exports = app => {
-  app.post(
-    "/api/surveys",
-    requireLogin,
-    requireCredits,
-    async (req, res) => {}
-  );
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipient
+        .split(",")
+        .map(email => ({ email }))
+        .trim(),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
